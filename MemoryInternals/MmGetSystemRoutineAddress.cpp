7badae52353b608a15d7df460d4c61f3d2341c51
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-07 00:07:32 +0200

    Update MmGetSystemRoutineAddress.cpp

--- MemoryInternals/MmGetSystemRoutineAddress.cpp
+++ MemoryInternals/MmGetSystemRoutineAddress.cpp
@@ -10,10 +10,10 @@ PVOID NTAPI MmGetSystemRoutineAddressx(PUNICODE_STRING SystemRoutineName)
 
 	for (SourceString = SystemRoutineName; RtlUnicodeStringToAnsiString(&DestinationString, SourceString, 1) < 0; SourceString = FunctionName) 
 		KeDelayExecutionThread(0, 0, (PLARGE_INTEGER)&MiShortTime);
-	SystemRoutineAddress = (PVOID)RtlFindExportedRoutineByName((INT64)PsNtosImageBase, DestinationString.Buffer);
+	SystemRoutineAddress = (PVOID)RtlFindExportedRoutineByName((INT64)PsNtosImageBase, DestinationString.Buffer);	// Finds The Function By Address Through Walking The PE Export Table Passing ntoskrnl.exe base address
 	
 	if (SystemRoutineAddress == nullptr) 
-		SystemRoutineAddress = (PVOID)RtlFindExportedRoutineByName(PsHalImageBase, DestinationString.Buffer);
+		SystemRoutineAddress = (PVOID)RtlFindExportedRoutineByName(PsHalImageBase, DestinationString.Buffer);	// If It Still a null pointer call the same function but passing Hal (Hardware Abstraction Layer) Base Address Not All Functions will be in ntoskrnl.exe
 	RtlFreeAnsiString((PANSI_STRING)&DestinationString);
 
 	if (SystemRoutineAddress && (int)MiMarkKernelCfgTarget(SystemRoutineAddress) < 0)
