2a007d0dde80f620913fd04ca0bac3883baf026f
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-07 14:08:24 +0200

    Create MiIsUserQueryVmCallerTrusted.cpp.cpp

---
+++ MemoryInternals/MiIsUserQueryVmCallerTrusted.cpp.cpp
@@ -0,0 +1,27 @@
+typedef struct _DMA_ADAPTER {
+  USHORT          Version;
+  USHORT          Size;
+  PDMA_OPERATIONS DmaOperations;
+} *PADAPTER_OBJECT, DMA_ADAPTER, *PDMA_ADAPTER;
+
+UINT MiIsUserQueryVmCallerTrusted(PACCESS_TOKEN ProcessToken)
+{
+    UINT IsTrusted {};
+    PSECURITY_IMPERSONATION_LEVEL  ImpersonationLevel{};
+    PTOKEN_TYPE TokenType; 
+    struct _DMA_ADAPTER *TokenReference; 
+    PBOOLEAN EffectiveOnly
+    
+    // Returns A Token Refernece To Be Used By HalPutDmaAdapter
+
+    // Returns A Pointer To The Effective Token of A Thread 
+    TokenReference = (_DMA_ADAPTER*)PsReferenceEffectiveToken(ProcessToken, &TokenType, &EffectiveOnly, &ImpersonationLevel, 0);
+
+    // SeTokenAdmin: Checks If Token Is Admin 
+    // SeSinglePrivilegeCheck: The SeSinglePrivilegeCheck routine checks for the passed privilege value in the context of the current thread 
+    // SeProfileSingleProcessPrivilege Luid of the privilage being checked
+    if(SeTokenAdmin(TokenReference) || SeSinglePrivilegeCheck(SeProfileSingleProcessPrivilege, 1))
+        ++IsTrusted;            // increments and set IsTrusted To 1 Means Its Trusted And Can Query VM 
+    HalPutDmaAdapter(TokenReference);
+    return IsTrusted;
+}
