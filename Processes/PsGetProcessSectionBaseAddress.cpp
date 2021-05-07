5505df2c97e1d198ba058730d5153b1b6b54babf
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-07 05:06:15 +0200

    Update PsGetProcessSectionBaseAddress.cpp

--- Processes/PsGetProcessSectionBaseAddress.cpp
+++ Processes/PsGetProcessSectionBaseAddress.cpp
@@ -1,3 +1,5 @@
+// Gets Base Address of A Process By Reading The SectionBaseAddress In EPROCESS 
+
 void* __fastcall PsGetProcessSectionBaseAddress(_EPROCESS *eproc)
 {
   return eproc->SectionBaseAddress;   
