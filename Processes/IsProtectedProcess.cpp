04d5ed3cc3f5d00882c21c1c3c257027ad6252fb
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-07 04:11:52 +0200

    Update IsProtectedProcess.cpp

--- Processes/IsProtectedProcess.cpp
+++ Processes/IsProtectedProcess.cpp
@@ -1,4 +1,12 @@
 bool __fastcall PsIsProtectedProcess(_EPROCESS* eproc)
 {
   return (*(BYTE*)(eproc + 0x87A) & 7) != 0;  //  +0x87a Protection: _PS_PROTECTION Ands with 7 and returns true if its 1 and not equal to zero 
+  
+  /*
+    IDA:
+          test    byte ptr [rcx+87Ah], 7      ; rcx = _EPROCESS 
+          mov     eax, 0
+          setnbe  al
+          retn
+  */ 
 }
