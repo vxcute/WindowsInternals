9e37fb48547ab40ab0fed7bf8724484198e59404
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-08 00:49:09 +0200

    Update PsGetProcessProtection.cpp

--- Processes/PsGetProcessProtection.cpp
+++ Processes/PsGetProcessProtection.cpp
@@ -1,10 +1,13 @@
-// Gets Process Protection Returns PPS_PROTECTION (Unexported)
+// Def: Gets Process Protection  
+// Return Value: Returns PPS_PROTECTION 
+// Status: Unexported
 
 PPS_PROTECTION PsGetProcessProtection(_EPROCESS *eproc)
 {
     return eproc->Protection;
-
+    
     /*  
+    IDA:
         mov     al, [rcx+87Ah]      0x87A offset to PPS_PROTECTION 
         retn
     */
