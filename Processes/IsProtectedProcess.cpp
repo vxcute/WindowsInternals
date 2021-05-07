0b828b594ad8e77bc706df9a2773cac3bbe8c2eb
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-07 04:08:15 +0200

    Delete IsProtectedProcess.cpp

--- Processes/IsProtectedProcess.cpp
+++
@@ -1,4 +0,0 @@
-bool __fastcall PsIsProtectedProcess(KPROCESS* kproc)
-{
-  return (*(BYTE*)(kproc + 0x87A) & 7) != 0;  //  +0x87a Protection : _PS_PROTECTION & 7 Returns True If Its Set to 1 and not equal to zero 
-}
