81cfdf89f949016fdaccafbaf0eb204f774109d4
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-08 19:53:11 +0200

    Update RtlFreeAnsiString.cpp

--- Strings/RtlFreeAnsiString.cpp
+++ Strings/RtlFreeAnsiString.cpp
@@ -3,9 +3,9 @@ void NTAPI RtlFreeAnsiString(PANSI_STRING AnsiString)
 	ULONG PoolTag; 
 	CHAR* AnsiStringBuffer; 
 
-	AnsiStringBuffer = AnsiString->Buffer;
+	AnsiStringBuffer = AnsiString->Buffer;			// PANSI_STRING Is Struct Contains A Variable Called Buffer Which Will Contain THe String 
 	
-	if (AnsiStringBuffer) 
-		ExFreePoolWithTag(AnsiStringBuffer, PoolTag);
-	*(&AnsiString) = nullptr;
+	if (AnsiStringBuffer) 				// If The Buffer Is Not Equal To Null 
+		ExFreePoolWithTag(AnsiStringBuffer, PoolTag);			// Free The Pool Allocated 
+	*(&AnsiString) = nullptr;			// Set AnsiString To null pointer 
 }
