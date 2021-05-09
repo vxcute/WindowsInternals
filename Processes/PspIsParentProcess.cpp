0a0b24132ec0c036cd5f2f5f0f79ddd9ec63fa9c
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-09 02:34:06 +0200

    Update PspIsParentProcess.cpp

--- Processes/PspIsParentProcess.cpp
+++ Processes/PspIsParentProcess.cpp
@@ -1,14 +1,14 @@
 // Def: Checks If Its Parent Process or not 
 // Status: Unexported | Undocumented 
 
-bool PspIsParentProcess(_PEPROCESS ParentProcess, _PEPROCESS ChildProcess)
+bool PspIsParentProcess(_PEPROCESS ProcessA, _PEPROCESS ProcessB)
 {
 
 	bool IsParentProcess = false;
 	
 	// If Its A Child Process Its InheritedFromUniqueProcessId Will Be Equal To Parent Process UniqueProcessId So Check For That 
 	
-	if (ChildProcess->InheritedFromUniqueProcessId == ParentProcess->UniqueProcessId)
+	if (ProcessB->InheritedFromUniqueProcessId == ProcessA->UniqueProcessId)
 
 		/*	
 		   More Checking Windows Keeps Tracks Of Processes Sequence Using This Sequence Number 
@@ -16,7 +16,7 @@ bool PspIsParentProcess(_PEPROCESS ParentProcess, _PEPROCESS ChildProcess)
 			 Parent Process Will Have A Sequence Number Less Than Of Child Process Sequence Number 
 			 So If Child Process Has Higher Sequence Number Set IsParentProcess To Be True 
 		 */ 
-		IsParentProcess = ChildProcess->SequenceNumber > ParentProcess->SequenceNumber;
+		IsParentProcess = ProcessB->SequenceNumber > ProcessA->SequenceNumber;
 	
 	return IsParentProcess;
 }
