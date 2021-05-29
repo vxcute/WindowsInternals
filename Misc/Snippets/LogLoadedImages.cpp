04cb00c172f8f50782bd259fa723f89b60b762d4
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-29 04:48:55 +0200

    Create LogLoadedImages.cpp

---
+++ Misc/Snippets/LogLoadedImages.cpp
@@ -0,0 +1,24 @@
+// Page 180 Excercise 3
+
+#include <ntddk.h>
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegPath);
+VOID Unload(PDRIVER_OBJECT DriverObject);
+VOID LogImage(PUNICODE_STRING FullImageName, HANDLE ProcessID, PIMAGE_INFO ImageInfo);
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegPath)
+{
+	UNREFERENCED_PARAMETER(RegPath);
+	PsSetLoadImageNotifyRoutine(LogImage);
+	return STATUS_SUCCESS;
+}
+
+VOID LogImage(PUNICODE_STRING FullImageName, HANDLE ProcessID, PIMAGE_INFO ImageInfo)
+{
+	DbgPrint("Image Loaded With Base Address: %p", ImageInfo->ImageBase);
+}
+
+VOID Unload(PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+}
