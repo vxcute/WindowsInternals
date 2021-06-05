567a0911dfe6413ac9a8a2173ded6a67f1c2022f
astr0 <68482847+vxcute@users.noreply.github.com>
2021-06-05 14:25:10 +0200

    Update KeUnstackDetachProcess.cpp

--- Reversed/Process/KeUnstackDetachProcess.cpp
+++ Reversed/Process/KeUnstackDetachProcess.cpp
@@ -1,9 +1,25 @@
 void NTAPI KeUnstackDetachProcess(PRKAPC_STATE ApcState)
 {
-	PKPROCESS Process; 
+	PKPROCESS Process;
+
+	// optain kprocess from PRKAPC_STATE structure 
 
 	Process = ApcState->Process;
-	
+
+	/*
+		 when calling KeStackAttachProcess to attach a process this function firstly checks if you are trying to attach to ur current process by 
+		 getting the CurrentProcess from the CurrentThread and checks if its equal to the passed process if the condition is true 
+		 it will then set the Process field to 1 and just exiting the function ending not attaching to the process. 
+
+		  if ( CurrentThread->ApcState.Process == Process )
+		  {
+			ApcState->Process = (PKPROCESS)1;
+		  }
+
+		  so here KeUnstackDetachProcess checks the Process is not equal to 1 (i.e. not the current process). 
+		  if it is it will de-attach the process using KiDetachProcess
+	*/
+
 	if (Process != (PKPROCESS)1)
 	{
 		if (!Process)
