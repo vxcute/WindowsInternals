fb08e58b06b05abb3d6d643015d11b75e057aec1
astr0 <68482847+vxcute@users.noreply.github.com>
2021-06-20 22:40:49 +0200

    Update UmGetNtosImageBase.cpp

--- Misc/Snippets/UserMode/UmGetNtosImageBase.cpp
+++ Misc/Snippets/UserMode/UmGetNtosImageBase.cpp
@@ -1,47 +1,75 @@
-#include "nt.hpp"
+#include <nt.hpp>
+#include <iostream>
+#include <Windows.h>
+#include <Psapi.h>
+
+#define ARRAY_SIZE 1024
+
+PVOID GetNtosImageBase1()
+{
+    LPVOID ImageBase[ARRAY_SIZE];
+
+    DWORD Needed;
+    
+    if (EnumDeviceDrivers(ImageBase, ARRAY_SIZE, &Needed))
+    {
+        return ImageBase[0];
+    }
+
+    return nullptr;
+}
 
 template <typename T>
-auto GetRoutineAddress(std::string routine_name, std::string module_name) -> T
+T GetRoutineAddress(PCSTR RoutineName, PCSTR ModuleName) 
 {
-    HMODULE mod = GetModuleHandleA(module_name.c_str());
-    if (mod) {
-        T RoutineAddress = (T)GetProcAddress(mod, routine_name.c_str());
-        if (RoutineAddress)
-            return RoutineAddress;
-        return nullptr;
-    }
-    return nullptr;
-}
+    HMODULE hModule = GetModuleHandleA(ModuleName);
 
-auto GetNtosImageBase1() -> PVOID
-{
-    PVOID NtImageBase;
-    _NtQuerySystemInformation NtQuerySystemInformation = GetRoutineAddress< _NtQuerySystemInformation>("NtQuerySystemInformation", "ntdll.dll");
-    PRTL_PROCESS_MODULES ModuleInfo = reinterpret_cast<PRTL_PROCESS_MODULES>(VirtualAlloc(NULL, 1024 * 1024, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
-    if (ModuleInfo) {
-        if (NT_SUCCESS(NtQuerySystemInformation(SystemModuleInformation, ModuleInfo, 1024 * 1024, nullptr)))
+    if (hModule)
+    {
+        T RoutineAddress = (T)GetProcAddress(hModule, RoutineName);
+
+        if (RoutineAddress)
         {
-            NtImageBase = ModuleInfo->Modules[0].ImageBase;
-            if (NtImageBase)
-                return NtImageBase;
-            return nullptr;
+            return RoutineAddress;
         }
     }
+
     return nullptr;
 }
 
-auto GetNtosImageBase2() -> PVOID
+PVOID GetNtosImageBase2()
 {
-    LPVOID lpImageBase[1024];
-    DWORD Needed;
-    if (EnumDeviceDrivers(lpImageBase, sizeof(lpImageBase), &Needed))
-        return lpImageBase[0];
+    PVOID NtosImageBase;
+
+    _NtQuerySystemInformation NtQuerySystemInformation;
+
+    PRTL_PROCESS_MODULES ModuleInfo;
+
+    NtQuerySystemInformation = GetRoutineAddress<_NtQuerySystemInformation>("NtQuerySystemInformation", "ntdll.dll");
+    
+    ModuleInfo = (PRTL_PROCESS_MODULES)VirtualAlloc(NULL, MEM_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    
+    if (ModuleInfo) 
+    {
+        if (NT_SUCCESS(NtQuerySystemInformation(SystemModuleInformation, ModuleInfo, MEM_SIZE, nullptr)))
+        {
+            NtosImageBase = ModuleInfo->Modules[0].ImageBase;
+            
+            if (NtosImageBase) 
+            {
+                VirtualFree(&ModuleInfo, MEM_SIZE, MEM_RELEASE | MEM_DECOMMIT);
+                return NtosImageBase;
+            }   
+        }
+    }
+
+    VirtualFree(&ModuleInfo, MEM_SIZE, MEM_RELEASE | MEM_DECOMMIT);
     return nullptr;
 }
 
 int main()
 {
-    std::cout << GetNtosImageBase1() << std::endl;
-    std::cout << GetNtosImageBase2() << std::endl;
-    return 0;
+    std::cout << GetNtosImageBase1() << " =====[+]===== " << GetNtosImageBase2() << std::endl;
+
+	return 0;
 }
