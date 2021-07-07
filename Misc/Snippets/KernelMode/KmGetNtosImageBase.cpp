891fda8ae83653de9dc71f5198783eeea36123f4
astr0 <68482847+vxcute@users.noreply.github.com>
2021-07-07 17:40:38 +0200

    Update KmGetNtosImageBase.cpp

--- Misc/Snippets/KernelMode/KmGetNtosImageBase.cpp
+++ Misc/Snippets/KernelMode/KmGetNtosImageBase.cpp
@@ -6,162 +6,128 @@
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegPath);
 VOID Unload(PDRIVER_OBJECT DriverObject);
 
-template <typename T>
-T GetKernelExport(PCWSTR zExportName)
+template <typename ExportType>
+ExportType GetKernelExport(PCWSTR zExportName)
 {
-	__try
-	{
-		UNICODE_STRING UExportName;
+	UNICODE_STRING UExportName;
 
-		RtlInitUnicodeString(&UExportName, zExportName);
+	RtlInitUnicodeString(&UExportName, zExportName);
 
-		T ExportAddress = (T)MmGetSystemRoutineAddress(&UExportName);
+	ExportType ExportAddress = (ExportType)MmGetSystemRoutineAddress(&UExportName);
 
-		return ExportAddress ? ExportAddress : T();
-
-	}
-
-	__except (EXCEPTION_EXECUTE_HANDLER) {}
+	return ExportAddress ? ExportAddress : ExportType(nullptr);
 }
 
 
 PVOID GetNtosImageBase1()
 {
-	__try 
+	PVOID NtosImageBase;
+	
+	PSYSTEM_MODULE_INFORMATION ModInfo;
+
+	_ZwQuerySystemInformation ZwQuerySystemInformation;
+
+	ZwQuerySystemInformation = GetKernelExport<_ZwQuerySystemInformation>(L"ZwQuerySystemInformation");
+
+	ModInfo = (PSYSTEM_MODULE_INFORMATION)ExAllocatePoolZero(NonPagedPool, POOL_SIZE, POOL_TAG);
+
+	if (ModInfo)
 	{
-		PVOID NtosImageBase;
-		
-		PSYSTEM_MODULE_INFORMATION ModInfo;
 
-		_ZwQuerySystemInformation ZwQuerySystemInformation;
-
-		ZwQuerySystemInformation = GetKernelExport<_ZwQuerySystemInformation>(L"ZwQuerySystemInformation");
-
-		ModInfo = (PSYSTEM_MODULE_INFORMATION)ExAllocatePoolZero(NonPagedPool, POOL_SIZE, POOL_TAG);
-
-		if (ModInfo)
+		if (NT_SUCCESS(ZwQuerySystemInformation(SystemModuleInformation, ModInfo, POOL_SIZE, nullptr)))
 		{
+			NtosImageBase = ModInfo->Module[0].ImageBase;
 
-			if (NT_SUCCESS(ZwQuerySystemInformation(SystemModuleInformation, ModInfo, POOL_SIZE, nullptr)))
+			if (NtosImageBase)
 			{
-				NtosImageBase = ModInfo->Module[0].ImageBase;
-
-				if (NtosImageBase)
-				{
-					ExFreePoolWithTag(ModInfo, POOL_TAG);
-					return NtosImageBase;
-				}
+				ExFreePoolWithTag(ModInfo, POOL_TAG);
+				return NtosImageBase;
 			}
 		}
-
-		ExFreePoolWithTag(ModInfo, POOL_TAG);
-		return nullptr;
 	}
+
+	ExFreePoolWithTag(ModInfo, POOL_TAG);
+	return nullptr;
 	
-	__except (EXCEPTION_EXECUTE_HANDLER) {}
 }
 
 PVOID GetNtosImageBase2()
 {
-	__try 
-	{
-		PVOID NtosImageBase;
+	PVOID NtosImageBase;
 
-		_RtlLookupFunctionEntry RtlLookupFunctionEntry;
+	_RtlLookupFunctionEntry RtlLookupFunctionEntry;
 
-		RtlLookupFunctionEntry = GetKernelExport<_RtlLookupFunctionEntry>(L"RtlLookupFunctionEntry");
+	RtlLookupFunctionEntry = GetKernelExport<_RtlLookupFunctionEntry>(L"RtlLookupFunctionEntry");
 
-		RtlLookupFunctionEntry((DWORD64)&MmCopyMemory, (PDWORD64)&NtosImageBase, nullptr);
+	RtlLookupFunctionEntry((DWORD64)&MmCopyMemory, (PDWORD64)&NtosImageBase, nullptr);
 
-		return NtosImageBase ? NtosImageBase : nullptr;
-	}
-	__except (EXCEPTION_EXECUTE_HANDLER) {}
+	return NtosImageBase ? NtosImageBase : nullptr;
 }
 
 
 PVOID GetNtosImageBase3()
 {
-	__try
-	{
-		PVOID NtosImageBase;
+	PVOID NtosImageBase;
 
-		_RtlPcToFileHeader RtlPcToFileHeader = GetKernelExport<_RtlPcToFileHeader>(L"RtlPcToFileHeader");
+	_RtlPcToFileHeader RtlPcToFileHeader = GetKernelExport<_RtlPcToFileHeader>(L"RtlPcToFileHeader");
 
-		RtlPcToFileHeader(&MmCopyMemory, &NtosImageBase);
+	RtlPcToFileHeader(&MmCopyMemory, &NtosImageBase);
 
-		return NtosImageBase ? NtosImageBase : nullptr;
-	}
-	__except (EXCEPTION_EXECUTE_HANDLER) {}
+	return NtosImageBase ? NtosImageBase : nullptr;
 }
 
 
 PVOID GetNtosImageBase4()
 {
-	__try
+	_KSPECIAL_REGISTERS SpecialRegisters;
+
+	__sidt(&SpecialRegisters.Idtr.Limit);
+
+	PIDT_ENTRY IdtBase = (PIDT_ENTRY)SpecialRegisters.Idtr.Base;
+
+	for (auto Page = (uintptr_t)IdtBase[0].InterruptServiceRoutine & ~0xfff;; Page; Page -= PAGE_SIZE)
 	{
-		_KSPECIAL_REGISTERS SpecialRegisters;
 
-		__sidt(&SpecialRegisters.Idtr.Limit);
-
-		PIDT_ENTRY IdtBase = (PIDT_ENTRY)SpecialRegisters.Idtr.Base;
-
-		auto Page = (uintptr_t)IdtBase[0].InterruptServiceRoutine & ~0xfff;
-
-		for (; Page; Page -= PAGE_SIZE)
+		if (*(USHORT*)Page == IMAGE_DOS_SIGNATURE)
 		{
 
-			if (*(USHORT*)Page == IMAGE_DOS_SIGNATURE)
+			for (auto Bytes = Page; Bytes < Page + 0x400; Bytes += 8)
 			{
-
-				for (auto Bytes = Page; Bytes < Page + 0x400; Bytes += 8)
+				if (*(ULONG64*)(Bytes) == PAGELK_PATTERN)
 				{
-					if (*(ULONG64*)(Bytes) == PAGELK_PATTERN)
-					{
-						return (PVOID)Page;
-					}
+					return (PVOID)Page;
 				}
 			}
 		}
-
-		return nullptr;
 	}
 
-	__except (EXCEPTION_EXECUTE_HANDLER) {}
+	return nullptr;
 }
 
 
 PVOID GetNtosImageBase5()
 {
-	__try
+	for (auto Page = __readmsr(IA32_LSTAR) & ~0xfff;; Page; Page -= PAGE_SIZE)
 	{
-		auto Page = __readmsr(IA32_LSTAR) & ~0xfff;
 
-		for (; Page; Page -= PAGE_SIZE)
+		if (*(USHORT*)Page == IMAGE_DOS_SIGNATURE)
 		{
 
-			if (*(USHORT*)Page == IMAGE_DOS_SIGNATURE)
+			for (auto Bytes = Page; Bytes < Page + 0x400; Bytes += 8)
 			{
-
-				for (auto Bytes = Page; Bytes < Page + 0x400; Bytes += 8)
+				if (*(ULONG64*)(Bytes) == PAGELK_PATTERN)
 				{
-					if (*(ULONG64*)(Bytes) == PAGELK_PATTERN)
-					{
-						return (PVOID)Page;
-					}
+					return (PVOID)Page;
 				}
 			}
 		}
-
-		return nullptr;
 	}
 
-	__except (EXCEPTION_EXECUTE_HANDLER) {}
+	return nullptr;
 }
-
 
 PVOID GetNtosImageBase6()
 {
-
 	PKLDR_DATA_TABLE_ENTRY NtosKldr;
 
 	PLIST_ENTRY PsLoadedModuleList;
