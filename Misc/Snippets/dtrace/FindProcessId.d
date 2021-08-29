14b541adadce0ebd96469f5b7ea4a0df36636908
astr0 <68482847+vxcute@users.noreply.github.com>
2021-08-28 19:07:13 -0700

    Update FindProcessId.d

--- Misc/Snippets/dtrace/FindProcessId.d
+++ Misc/Snippets/dtrace/FindProcessId.d
@@ -6,30 +6,25 @@ struct nt`_EPROCESS* CurrentEProcess;
 
 BEGIN
 {
-    system("cls");
     CurrentEProcessPtr = (uintptr_t)((struct nt`_LIST_ENTRY*)(void*)&nt`PsActiveProcessHead)->Flink;
-    Found = 0;
 }
 
-tick-1ms  / Found == 0 /
+tick-1ms  
 {
-    if(Found == 0)
+    CurrentEProcess = (struct nt`_EPROCESS*)(CurrentEProcessPtr - offsetof(nt`_EPROCESS, ActiveProcessLinks));
+
+    ProcessName = (string)CurrentEProcess->ImageFileName;
+
+    ProcessId = (uintptr_t)CurrentEProcess->UniqueProcessId;
+
+    if(ProcessName == $1)
     {
-        CurrentEProcess = (struct nt`_EPROCESS*)(CurrentEProcessPtr - offsetof(nt`_EPROCESS, ActiveProcessLinks));
+        printf("[+] Found %s (PID: %d)\n", ProcessName, ProcessId);
+        exit(0);
+    }
 
-        ProcessName = (string)CurrentEProcess->ImageFileName;
-
-        ProcessId = (uintptr_t)CurrentEProcess->UniqueProcessId;
-
-        if(ProcessName == $1)
-        {
-            printf("[+] Found %s (PID: %d)\n", ProcessName, ProcessId);
-            Found = 1;
-        }
-
-        else
-        {
-            CurrentEProcessPtr = (uintptr_t)((struct nt`_LIST_ENTRY*)(void*)CurrentEProcessPtr)->Flink;
-        }
+    else
+    {
+        CurrentEProcessPtr = (uintptr_t)((struct nt`_LIST_ENTRY*)(void*)CurrentEProcessPtr)->Flink;
     }
 }
